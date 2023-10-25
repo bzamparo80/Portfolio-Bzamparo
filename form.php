a3b0aa38b4b70fc2de0a135c0991bd629e6840f3
bzamparo80 <bzamparo@gmail.com>
2023-10-25 09:38:29 -0300

    form

--- form.php
+++ form.php
@@ -1,14 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nombre = $_POST["nombre"];
+    $telefono = $_POST["telefono"];
     $correo = $_POST["correo"];
+    $mensaje = $_POST["mensaje"];
+
     // Otros campos del formulario
 
     $destinatario = "bzamparo@gmail.com";
     $asunto = "Mensaje desde tu sitio web";
 
     $mensaje = "Nombre: $nombre\n";
+    $mensaje .= "Telefono: $telefono\n";
     $mensaje .= "Correo: $correo\n";
+    $mensaje .= "Mensaje: $mensaje\n";
+
     // Agregar otros campos del formulario al mensaje
 
     $headers = "From: $correo";
